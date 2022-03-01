74be9b94994a08c45c2aec217e1b2f6809690dc7
jasonxuyang <jasonxuyang@gmail.com>
2022-02-28 22:41:12 -0500

    setup global styles

---
+++ pages/home.js
@@ -0,0 +1,13 @@
+import "../styles/home.module.scss";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h1>Hello World my name is jason</h1>
+        <p>hi</p>
+        <p>Hello my name is jason</p>
+      </main>
+    </div>
+  );
+}
